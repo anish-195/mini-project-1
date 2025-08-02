6a049f83b03cf7caba55e74a7d6e214fc5894cdc
Adarsh <adarshkumar0278@gmail.com>
2025-08-02 23:10:53 +0530

    data print feacture

--- app.js
+++ app.js
@@ -4,11 +4,34 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+
 
 app.listen(3000, () => {
-  console.log("It's running");
+  console.log('Server is running on http://localhost:3000');
 });
 
+let notes = [{
+    "id": 3,
+    "title": "python",
+    "content": "Try creating routes and using middleware"
+  }];
+let currentId = 3;
+
+
+app.get("/", (req, res) => {
+  res.send('hello');
+});
+
+
+
+app.get("/notes/:id", (req, res) => {
+  
+const id = parseInt(req.params.id);
+  const note = notes.find(n => n.id === id);
+  
+  if (!note) {
+    return res.status(404).json({ message: 'Note not found' });
+  }
+
+  res.json(note);
+});
